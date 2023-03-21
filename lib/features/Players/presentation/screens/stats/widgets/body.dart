0a0c784de72bdebae91dcc14aa542b0486e6076a
uroojmajid2000 <uroojmajid2000>
2023-03-21 19:37:14 +0500

    events work

--- lib/features/Players/presentation/screens/stats/widgets/body.dart
+++ lib/features/Players/presentation/screens/stats/widgets/body.dart
@@ -10,262 +10,193 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('data'),
-        DataTable(
-          columnSpacing: 10,
-          headingRowColor: MaterialStateProperty.all(Colors.green[300]),
-          decoration: BoxDecoration(
-            border: Border(
-              right: BorderSide(
-                color: Colors.grey,
-                width: 2,
-              ),
-            ),
+    return Column(children: [
+      Text('data'),
+      Container(
+        width: double.infinity,
+        height: 400,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromRGBO(210, 143, 82, 0.2),
+              Color.fromRGBO(0, 163, 46, 0.01),
+            ],
+           
           ),
-          columns: [
-            DataColumn(label: Text('Team')),
-          ],
-          rows: [
-            ...teamsData.map((team) => DataRow(
-                  cells: [
-                    DataCell(Text(
-                      '${team.position.toString()}  ${team.name}',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )),
-                  ],
-                ))
-          ],
-        )
-      ],
-    );
+        ),
+      )
+
+      // Expanded(
+      //     child: SingleChildScrollView(
+      //   scrollDirection: Axis.horizontal,
+      //   child: SizedBox(
+      //     height: 300,
+      //     child: DataTable(
+      //         dividerThickness: 1.5,
+      //         columnSpacing: 40,
+      //         decoration: BoxDecoration(
+      //           border: Border.all(
+      //             color: Colors.grey,
+      //             width: 1,
+      //             style: BorderStyle.solid,
+      //           ),
+      //         ),
+      //         columns: [
+      //           DataColumn(
+      //             label: Text('PLAYER'),
+      //             numeric: false,
+      //           ),
+      //           DataColumn(
+      //               label: VerticalDivider(
+      //             color: Colors.grey,
+      //             width: 1,
+      //           )),
+      //           DataColumn(
+      //             label: Text('PTS'),
+      //             numeric: true,
+      //           ),
+      //           DataColumn(
+      //               label: VerticalDivider(
+      //             color: Colors.grey,
+      //             width: 1,
+      //           )),
+      //           DataColumn(
+      //             label: Text('AST'),
+      //             numeric: true,
+      //           ),
+      //           DataColumn(
+      //               label: VerticalDivider(
+      //             color: Colors.grey,
+      //             width: 1,
+      //           )),
+      //           DataColumn(
+      //             label: Text('WINSCORE'),
+      //             numeric: true,
+      //           ),
+      //           DataColumn(
+      //               label: VerticalDivider(
+      //             color: Colors.grey,
+      //             width: 1,
+      //           )),
+      //           DataColumn(
+      //             label: Text('AST2'),
+      //             numeric: true,
+      //           ),
+      //           DataColumn(
+      //               label: VerticalDivider(
+      //             color: Colors.grey,
+      //             width: 1,
+      //           )),
+      //           DataColumn(
+      //             label: Text('AST3'),
+      //             numeric: true,
+      //           ),
+      //         ],
+      //         rows: [
+      //           ...teamsData.map((team) => DataRow(
+      //                 cells: [
+      //                   DataCell(
+      //                     Container(
+      //                         alignment: AlignmentDirectional.center,
+      //                         child: Text(
+      //                           team.PLAYER.toString(),
+      //                           style: TextStyle(fontWeight: FontWeight.bold),
+      //                         )),
+      //                   ),
+      //                   DataCell(VerticalDivider(
+      //                     color: Colors.grey,
+      //                     width: 1,
+      //                   )),
+      //                   DataCell(
+      //                     Container(
+      //                         alignment: AlignmentDirectional.center,
+      //                         child: Text(team.PTS.toString())),
+      //                   ),
+      //                   DataCell(VerticalDivider(
+      //                     color: Colors.grey,
+      //                     width: 1,
+      //                   )),
+      //                   DataCell(
+      //                     Container(
+      //                         alignment: AlignmentDirectional.center,
+      //                         child: Text(team.AST.toString())),
+      //                   ),
+      //                   DataCell(VerticalDivider(
+      //                     color: Colors.grey,
+      //                     width: 1,
+      //                   )),
+      //                   DataCell(
+      //                     Container(
+      //                         alignment: AlignmentDirectional.center,
+      //                         child: Text(team.WINSCORE.toString())),
+      //                   ),
+      //                   DataCell(VerticalDivider(
+      //                     color: Colors.grey,
+      //                     width: 1,
+      //                   )),
+      //                   DataCell(
+      //                     Container(
+      //                         alignment: AlignmentDirectional.center,
+      //                         child: Text(team.AST2.toString())),
+      //                   ),
+      //                   DataCell(VerticalDivider(
+      //                     color: Colors.grey,
+      //                     width: 1,
+      //                   )),
+      //                   DataCell(
+      //                     Container(
+      //                         alignment: AlignmentDirectional.center,
+      //                         child: Text(team.AST3.toString())),
+      //                   ),
+      //                 ],
+      //               ))
+      //         ]),
+      //   ),
+      // ))
+    ]);
   }
 }
 
 List<Team> teamsData = [
   Team(
-      position: 1,
-      name: 'Atletico Madrid',
-      points: 11,
-      played: 5,
-      won: 3,
-      drawn: 2,
-      lost: 0,
-      against: 4,
-      gd: 3),
+      PLAYER: "Elijah Oliver C",
+      PTS: 5,
+      AST: 8,
+      WINSCORE: 9,
+      AST2: 12,
+      AST3: 15),
   Team(
-      position: 2,
-      name: 'Real Madrid',
-      points: 10,
-      played: 4,
-      won: 3,
-      drawn: 1,
-      lost: 0,
-      against: 6,
-      gd: 7),
+      PLAYER: "Elijah Oliver C",
+      PTS: 5,
+      AST: 8,
+      WINSCORE: 9,
+      AST2: 12,
+      AST3: 15),
   Team(
-      position: 3,
-      name: 'Valencia',
-      points: 10,
-      played: 4,
-      won: 3,
-      drawn: 2,
-      lost: 0,
-      against: 2,
-      gd: 7),
-  Team(
-      position: 4,
-      name: 'Athletic Bilbao',
-      points: 9,
-      played: 5,
-      won: 2,
-      drawn: 3,
-      lost: 0,
-      against: 1,
-      gd: 3),
-  Team(
-      position: 5,
-      name: 'Real Sociedad',
-      points: 9,
-      played: 4,
-      won: 3,
-      drawn: 0,
-      lost: 1,
-      against: 4,
-      gd: 2),
-  Team(
-      position: 6,
-      name: 'Osasuna',
-      points: 8,
-      played: 5,
-      won: 2,
-      drawn: 2,
-      lost: 1,
-      against: 6,
-      gd: 0),
-  Team(
-      position: 7,
-      name: 'Mallorca',
-      points: 8,
-      played: 5,
-      won: 2,
-      drawn: 2,
-      lost: 1,
-      against: 3,
-      gd: 0),
-  Team(
-      position: 8,
-      name: 'Sevilla',
-      points: 7,
-      played: 3,
-      won: 2,
-      drawn: 1,
-      lost: 0,
-      against: 1,
-      gd: 4),
-  Team(
-      position: 9,
-      name: 'Rayo Vallecano',
-      points: 7,
-      played: 5,
-      won: 2,
-      drawn: 1,
-      lost: 2,
-      against: 5,
-      gd: 3),
-  Team(
-      position: 10,
-      name: 'Barcelona',
-      points: 7,
-      played: 3,
-      won: 2,
-      drawn: 1,
-      lost: 0,
-      against: 4,
-      gd: 3),
-  Team(
-      position: 11,
-      name: 'Elche',
-      points: 6,
-      played: 5,
-      won: 1,
-      drawn: 3,
-      lost: 1,
-      against: 3,
-      gd: 0),
-  Team(
-      position: 12,
-      name: 'Real Betis',
-      points: 5,
-      played: 4,
-      won: 1,
-      drawn: 2,
-      lost: 1,
-      against: 4,
-      gd: 0),
-  Team(
-      position: 13,
-      name: 'Cadiz',
-      points: 5,
-      played: 5,
-      won: 1,
-      drawn: 2,
-      lost: 2,
-      against: 8,
-      gd: -2),
-  Team(
-      position: 14,
-      name: 'Villarreal',
-      points: 4,
-      played: 4,
-      won: 0,
-      drawn: 4,
-      lost: 0,
-      against: 2,
-      gd: 0),
-  Team(
-      position: 15,
-      name: 'Levante',
-      points: 4,
-      played: 5,
-      won: 0,
-      drawn: 4,
-      lost: 1,
-      against: 7,
-      gd: -1),
-  Team(
-      position: 16,
-      name: 'Espanyol',
-      points: 2,
-      played: 4,
-      won: 0,
-      drawn: 2,
-      lost: 2,
-      against: 3,
-      gd: -2),
-  Team(
-      position: 17,
-      name: 'Granada',
-      points: 2,
-      played: 4,
-      won: 0,
-      drawn: 2,
-      lost: 2,
-      against: 7,
-      gd: -5),
-  Team(
-      position: 18,
-      name: 'Celta',
-      points: 1,
-      played: 5,
-      won: 0,
-      drawn: 1,
-      lost: 4,
-      against: 10,
-      gd: -6),
-  Team(
-      position: 19,
-      name: 'Getafe',
-      points: 0,
-      played: 5,
-      won: 0,
-      drawn: 0,
-      lost: 5,
-      against: 8,
-      gd: -7),
-  Team(
-      position: 20,
-      name: 'Alaves',
-      points: 0,
-      played: 4,
-      won: 0,
-      drawn: 0,
-      lost: 4,
-      against: 10,
-      gd: -9),
+      PLAYER: "Elijah Oliver C",
+      PTS: 5,
+      AST: 8,
+      WINSCORE: 9,
+      AST2: 12,
+      AST3: 15),
 ];
 
 class Team {
   Team({
-    required this.position,
-    required this.name,
-    required this.points,
-    required this.played,
-    required this.won,
-    required this.drawn,
-    required this.lost,
-    required this.against,
-    required this.gd,
+    required this.PLAYER,
+    required this.PTS,
+    required this.AST,
+    required this.WINSCORE,
+    required this.AST2,
+    required this.AST3,
   });
 
-  final int position;
-  final String name;
-  final int points;
-  final int played;
-  final int won;
-  final int drawn;
-  final int lost;
-  final int against;
-  final int gd;
+  final String PLAYER;
+  final int PTS;
+  final int AST;
+  final int WINSCORE;
+  final int AST2;
+  final int AST3;
 }
