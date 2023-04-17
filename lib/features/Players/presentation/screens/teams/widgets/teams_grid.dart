3d1cce1a8782992bf428b4027db327d2765ab4df
uroojmajid2000 <urooj.majid2000@gmail.com>
2023-04-17 10:32:48 +0500

    players teams add

--- lib/features/Players/presentation/screens/teams/widgets/teams_grid.dart
+++ lib/features/Players/presentation/screens/teams/widgets/teams_grid.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:statboard_flutter_app/features/Business/Presentation/screens/ballteams/ball_teams.dart';
+import 'package:statboard_flutter_app/features/Players/presentation/screens/allteams/all_teams.dart';
 import 'package:statboard_flutter_app/features/Players/presentation/screens/teams/widgets/teams_container.dart';
 
 import '../../../../../../shared/routes/navigate.dart';
@@ -17,7 +17,7 @@ class TeamsList extends StatelessWidget {
         itemCount: teamNames.length,
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
-              onTap: () => Navigate.to(context, BAllTeams.id),
+              onTap: () => Navigate.to(context, AllTeams.id),
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8),
                 child: TeamsContainer(teamName: teamNames[index]),
