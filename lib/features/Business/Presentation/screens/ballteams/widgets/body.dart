7bb98476fe08f645815e163949a55ad0f9f24d8c
uroojmajid2000 <urooj.majid2000@gmail.com>
2023-04-17 16:35:45 +0500

    business flow floating action button remove

--- lib/features/Business/Presentation/screens/ballteams/widgets/body.dart
+++ lib/features/Business/Presentation/screens/ballteams/widgets/body.dart
@@ -16,192 +16,192 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              backgroundColor: kPrimaryColor,
-              child: SvgPicture.asset('assets/icons/edit.svg'),
-              // child:
-              //  Text(
-              //   '+',
-              //   style: TextStyle(color: Colors.white, fontSize: 45),
-              // ),
-              heroTag: null,
-              onPressed: () {
-                Navigate.to(context, BEditTeamsScreen.id);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FloatingActionButton(
-              backgroundColor: kPrimaryColor,
-              child: SvgPicture.asset('assets/icons/add.svg'),
-              // Text(
-              //   '-',
-              //   style: TextStyle(color: Colors.white, fontSize: 45),
-              // ),
-              heroTag: null,
-              onPressed: () {
-                Navigate.to(context, BAddTeamsScreen.id);
-              },
-            ),
-          ],
-        ),
+        // floatingActionButton: Column(
+        //   mainAxisAlignment: MainAxisAlignment.end,
+        //   children: <Widget>[
+        //     FloatingActionButton(
+        //       backgroundColor: kPrimaryColor,
+        //       child: SvgPicture.asset('assets/icons/edit.svg'),
+        //       // child:
+        //       //  Text(
+        //       //   '+',
+        //       //   style: TextStyle(color: Colors.white, fontSize: 45),
+        //       // ),
+        //       heroTag: null,
+        //       onPressed: () {
+        //         Navigate.to(context, BEditTeamsScreen.id);
+        //       },
+        //     ),
+        //     SizedBox(
+        //       height: 10,
+        //     ),
+        //     FloatingActionButton(
+        //       backgroundColor: kPrimaryColor,
+        //       child: SvgPicture.asset('assets/icons/add.svg'),
+        //       // Text(
+        //       //   '-',
+        //       //   style: TextStyle(color: Colors.white, fontSize: 45),
+        //       // ),
+        //       heroTag: null,
+        //       onPressed: () {
+        //         Navigate.to(context, BAddTeamsScreen.id);
+        //       },
+        //     ),
+        //   ],
+        // ),
         body: SingleChildScrollView(
             // scrollDirection: Axis.vertical,
             child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 14),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      padding: const EdgeInsets.symmetric(horizontal: 14),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+            DetailsTeams(),
+            SizedBox(
+              height: 14,
+            ),
+            AllTeamsContainer(),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
               children: [
-                SizedBox(
-                  height: 15,
-                ),
-                DetailsTeams(),
-                SizedBox(
-                  height: 14,
-                ),
-                AllTeamsContainer(),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Filters(
-                      text: 'Recent Matches',
-                      isSelected: _recentMatchesSelected,
-                      onChanged: (value) {
-                        setState(() {
-                          _recentMatchesSelected = value;
-                          _playersSelected = false;
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      width: 6,
-                    ),
-                    Filters(
-                      text: 'Players',
-                      isSelected: _playersSelected,
-                      onChanged: (value) {
-                        setState(() {
-                          _playersSelected = value;
-                          _recentMatchesSelected = false;
-                        });
-                      },
-                    ),
-                  ],
+                Filters(
+                  text: 'Recent Matches',
+                  isSelected: _recentMatchesSelected,
+                  onChanged: (value) {
+                    setState(() {
+                      _recentMatchesSelected = value;
+                      _playersSelected = false;
+                    });
+                  },
                 ),
                 SizedBox(
-                  height: 20,
+                  width: 6,
                 ),
-                if (_recentMatchesSelected)
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                Filters(
+                  text: 'Players',
+                  isSelected: _playersSelected,
+                  onChanged: (value) {
+                    setState(() {
+                      _playersSelected = value;
+                      _recentMatchesSelected = false;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            if (_recentMatchesSelected)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MyText.HeadingText(
+                    'Matches',
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  // GridView.builder(
+                  //   shrinkWrap: true,
+                  //   physics: const NeverScrollableScrollPhysics(),
+                  //   itemCount: 3,
+                  //   gridDelegate:
+                  //       const SliverGridDelegateWithFixedCrossAxisCount(
+                  //     crossAxisCount: 1,
+                  //     mainAxisSpacing: 20,
+                  //     childAspectRatio: 5 / 2,
+                  //   ),
+                  //   itemBuilder: (BuildContext context, int index) {
+                  //     return GestureDetector(
+                  //       onTap: () {
+                  //         // Navigate.next(context, AllTeams.id);
+                  //       },
+                  //       child: GridTile(
+                  //         child: RecentMatchesTeams(),
+                  //       ),
+                  //     );
+                  //   },
+                  // ),
+
+                  RecentMatchesTeams(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RecentMatchesTeams(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RecentMatchesTeams(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  // MatchesList(),
+                ],
+              ),
+            if (_playersSelected)
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       MyText.HeadingText(
-                        'Matches',
+                        "All",
                       ),
-                      SizedBox(
-                        height: 10,
+                      Text(
+                        "View Team Player Stats",
+                        style: TextStyle(
+                          color: Color(0xff00b0f0),
+                          fontSize: 10,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                      // GridView.builder(
-                      //   shrinkWrap: true,
-                      //   physics: const NeverScrollableScrollPhysics(),
-                      //   itemCount: 3,
-                      //   gridDelegate:
-                      //       const SliverGridDelegateWithFixedCrossAxisCount(
-                      //     crossAxisCount: 1,
-                      //     mainAxisSpacing: 20,
-                      //     childAspectRatio: 5 / 2,
-                      //   ),
-                      //   itemBuilder: (BuildContext context, int index) {
-                      //     return GestureDetector(
-                      //       onTap: () {
-                      //         // Navigate.next(context, AllTeams.id);
-                      //       },
-                      //       child: GridTile(
-                      //         child: RecentMatchesTeams(),
-                      //       ),
-                      //     );
-                      //   },
-                      // ),
-
-                      RecentMatchesTeams(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      RecentMatchesTeams(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      RecentMatchesTeams(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      // MatchesList(),
                     ],
                   ),
-                if (_playersSelected)
-                  Column(
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          MyText.HeadingText(
-                            "All",
-                          ),
-                          Text(
-                            "View Team Player Stats",
-                            style: TextStyle(
-                              color: Color(0xff00b0f0),
-                              fontSize: 10,
-                              fontFamily: "Montserrat",
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 14,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ProfileContainer(),
-                          ProfileContainer(),
-                          ProfileContainer(),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ProfileContainer(),
-                          ProfileContainer(),
-                          ProfileContainer(),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ProfileContainer(),
-                          ProfileContainer(),
-                          ProfileContainer(),
-                        ],
-                      ),
+                      ProfileContainer(),
+                      ProfileContainer(),
+                      ProfileContainer(),
                     ],
                   ),
-              ]),
-        )));
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProfileContainer(),
+                      ProfileContainer(),
+                      ProfileContainer(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProfileContainer(),
+                      ProfileContainer(),
+                      ProfileContainer(),
+                    ],
+                  ),
+                ],
+              ),
+          ]),
+    )));
   }
 }
--- lib/features/Business/Presentation/screens/bteams/widgets/body.dart
+++ lib/features/Business/Presentation/screens/bteams/widgets/body.dart
@@ -16,44 +16,44 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton:
-            //  Positioned(
-            //   bottom: 20,
-            //   right: 20,
-            //   child:
-            FloatingActionButton(
-          backgroundColor: kPrimaryColor,
-          child: Text(
-            '+',
-            style: TextStyle(color: Colors.white, fontSize: 45),
-          ),
-          onPressed: () {
-            Navigate.to(context, BTeamsDetails.id);
-          },
-        ),
+        // floatingActionButton:
+        //     //  Positioned(
+        //     //   bottom: 20,
+        //     //   right: 20,
+        //     //   child:
+        //     FloatingActionButton(
+        //   backgroundColor: kPrimaryColor,
+        //   child: Text(
+        //     '+',
+        //     style: TextStyle(color: Colors.white, fontSize: 45),
+        //   ),
+        //   onPressed: () {
+        //     Navigate.to(context, BTeamsDetails.id);
+        //   },
         // ),
+
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 14),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 25),
-              MySearchBar(
-                hintText: 'Search teams',
-                onChange: (value) {},
-                prefixIcon: 'assets/icons/search.png',
-                suffixIcon: 'assets/icons/filter.png',
-              ),
-              const SizedBox(height: 25),
-              MyText.HeadingText(
-                "All ",
-              ),
-              const SizedBox(height: 20),
-              TeamsList(teamNames: _teamNames),
-              const SizedBox(height: 50),
-            ],
+      padding: const EdgeInsets.symmetric(horizontal: 14),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 25),
+          MySearchBar(
+            hintText: 'Search teams',
+            onChange: (value) {},
+            prefixIcon: 'assets/icons/search.png',
+            suffixIcon: 'assets/icons/filter.png',
           ),
-        ));
+          const SizedBox(height: 25),
+          MyText.HeadingText(
+            "All ",
+          ),
+          const SizedBox(height: 20),
+          TeamsList(teamNames: _teamNames),
+          const SizedBox(height: 50),
+        ],
+      ),
+    ));
   }
 }
